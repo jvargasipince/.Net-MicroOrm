161c420025b4bc4b0c2507dd87f3da77d2cf59a1
Jorge Vargas <jorge.vargas@tss.com.pe>
2017-06-09 12:12:26 -0500

    Add project files.

---
+++ DemoMicroORMs/DataBase/DataBase/dbo/Tables/Companies.sql
@@ -0,0 +1,8 @@
+﻿create table company (
+id int identity(1,1) primary key,
+name nvarchar(100) not null,
+address nvarchar(100) null,
+phone nvarchar(10) null,
+status bit default 1
+)
+go
---
+++ DemoMicroORMs/DataBase/DataBase/dbo/Tables/Invoices.sql
@@ -0,0 +1,11 @@
+﻿create table invoice (
+id int identity(1,1) primary key,
+nroinvoice char(5) not null,
+companyid int foreign key references company(id),
+customer nvarchar(100) not null,
+ammount money not null,
+nroproducts int not null,
+datecreate datetime default getdate(),
+status bit default 1
+)
+go
